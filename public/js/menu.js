da775cc9714d0612d7b97783b73b8a3326183241
rtrevinnoc <rtrevinnoc@wearebuildingthefuture.com>
2022-05-03 00:47:44 -0500

    Testing]

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,11 +1,13 @@
-$(function(){
+$(document).ready(function(){
 	const nombre = $('empleadoNombre');
-	nombre.on("input", function() {
-		console.log(this.value)
-		/*$.getJSON('/verEmpleado', {
-		    empleado: this.value
-		  }, function(data) {
-		    console.log(data);
-		  });*/
-	});
-});
+	alert("hi");
+	nombre.value = "hi";
+	//nombre.on("input", function() {
+		//console.log(this.value)
+		//[>$.getJSON('/verEmpleado', {
+		    //empleado: this.value
+		  //}, function(data) {
+		    //console.log(data);
+		  //});*/
+	//});
+})
